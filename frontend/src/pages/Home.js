78ea42262f338c5a137cbf7f2d52858600c3dde3
nseah21 <nseah21@gmail.com>
2022-07-10 09:25:32 +0800

    Resolve warnings

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,31 +2,10 @@ import React, { useState } from 'react'
 import { SearchBar } from 'components'
 
 export default function Home() {
-  const [data, setData] = useState([])
-  const fetchData = () => {}
 
   return (
     <>
       <SearchBar />
-      {data.map((report, index) => {
-        return (
-          <div key={index}>
-            <h1>RECENT SCAMS</h1>
-            <div>
-              <strong>Scammer number:</strong>
-              {report.number}
-            </div>
-            <div>
-              <strong>Scammer website:</strong>
-              {report.website}
-            </div>
-            <div>
-              <strong>Victim story:</strong>
-              {report.story}
-            </div>
-          </div>
-        )
-      })}
     </>
   )
 }
