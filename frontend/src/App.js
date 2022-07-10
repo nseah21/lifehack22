7d97aae87d4905e4e20b3fd87945ab53d95ad84a
misterpuffin <snghaoren@gmail.com>
2022-07-10 11:02:52 +0800

    Add Notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import './App.css'
 import { Home, SubmitReport } from 'pages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Navbar } from 'components'
+import { Navbar, NotificationBar, NotificationProvider } from 'components'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='report' element={<SubmitReport />} />
-      </Routes>
-    </BrowserRouter>
+    <NotificationProvider>
+      <BrowserRouter>
+        <NotificationBar />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='report' element={<SubmitReport />} />
+        </Routes>
+      </BrowserRouter>
+    </NotificationProvider>
   )
 }
 
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,16 @@ import SearchBar from './SearchBar/SearchBar'
 import Navbar from './Navbar/Navbar'
 import Button from './Button/Button'
 import ScamDetails from './ScamDetails/ScamDetails'
+import { NotificationProvider } from './Message/MessageProvider'
+import NotificationContext from './Message/MessageProvider'
+import NotificationBar from './Message/NotificationBar'
 
-export { SearchBar, Navbar, Button, ScamDetails }
+export {
+  SearchBar,
+  Navbar,
+  Button,
+  ScamDetails,
+  NotificationProvider,
+  NotificationContext,
+  NotificationBar,
+}
